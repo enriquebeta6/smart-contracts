c51ece4767b57a9fc96254f3563ca3dac790d4be
Enrique Mora <enriquebeta7@gmail.com>
2022-06-24 17:21:28 -0300

    chore: add goerli network and change hardhat chainId

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -25,6 +25,9 @@ task('accounts', 'Prints the list of accounts', async (_, hre) => {
 const config: HardhatUserConfig = {
   solidity: '0.8.4',
   networks: {
+    hardhat: {
+      chainId: 1337,
+    },
     mainnet: {
       url: process.env.MAINNET_RPC_URL || '',
       accounts: process.env.MAINNET_PRIVATE_KEY
@@ -35,6 +38,12 @@ const config: HardhatUserConfig = {
       url: process.env.TESTNET_RPC_URL || '',
       accounts: process.env.TESTNET_PRIVATE_KEY
         ? [process.env.TESTNET_PRIVATE_KEY]
+        : [],
+    },
+    goerli: {
+      url: process.env.GOERLI_RPC_URL || '',
+      accounts: process.env.GOERLI_PRIVATE_KEY
+        ? [process.env.GOERLI_PRIVATE_KEY]
         : [],
     },
   },
