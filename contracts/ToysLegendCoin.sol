e164d50a99a402bcdb8f3b3eb9c83997f5c69bd5
Enrique Mora <enriquebeta7@gmail.com>
2023-11-17 23:45:56 -0300

    feat: update with last code

--- contracts/ToysLegendCoin.sol
+++ contracts/ToysLegendCoin.sol
@@ -4,7 +4,29 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ToysLegendCoin is ERC20 {
-    constructor(address _owner) ERC20("Toys Legend Coin", "TLC") {
-        _mint(_owner, 50_000_000 ether);
+    address public rewardAddress;
+    address public marketingAddress;
+    address public operationsAddress;
+    address public dynamicFlowAdress;
+    address public developmentAddress;
+
+    constructor(
+        address _rewardAddress,
+        address _marketingAddress,
+        address _operationsAddress,
+        address _dynamicFlowAdress,
+        address _developmentAddress
+    ) ERC20("Toys Legend Coin", "TLC") {
+        rewardAddress = _rewardAddress;
+        marketingAddress = _marketingAddress;
+        operationsAddress = _operationsAddress;
+        dynamicFlowAdress = _dynamicFlowAdress;
+        developmentAddress = _developmentAddress;
+
+        _mint(rewardAddress, 35_000_000 ether);
+        _mint(marketingAddress, 1_000_000 ether);
+        _mint(operationsAddress, 5_000_000 ether);
+        _mint(dynamicFlowAdress, 5_000_000 ether);
+        _mint(developmentAddress, 4_000_000 ether);
     }
 }
